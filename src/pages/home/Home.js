c47802f55b8025be22d1c7c8cd413de90d8e1909
brd310 <63228054+brd310@users.noreply.github.com>
2021-08-10 22:03:48 +0200

    fond+txt

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="Home_">
+            <section>
+                <h1>Bertrand Menel</h1>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
