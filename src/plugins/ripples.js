37d8238739dab4b0c6b3a100e50e4594b92184d9
brd310 <63228054+brd310@users.noreply.github.com>
2021-08-22 16:47:13 +0200

    nav+pluggins

---
+++ src/plugins/ripples.js
@@ -0,0 +1,17 @@
+import * as $ from 'jquery';
+
+$(".Home_").ripples({
+    resolution: 257,
+    dropRadius: 20,
+    perturbance: 0.01,
+});
+// try {
+//     $('.Home_').ripples({
+//         resolution: 251,
+//         dropRadius: 20,
+//         perturbance: 0.01,
+//     });
+// }
+// catch (e) {
+//     $('.error').show().text(e);
+// }
